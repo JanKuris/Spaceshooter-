db95f8c59857f54da21e874d986a4603a5e3472c
Jan <jan.kuris@gmail.com>
2023-03-31 23:28:13 +0200

    this is my firsth project. space shooter... now i have isue with move

---
+++ config/consts.py
@@ -0,0 +1,26 @@
+
+GAME_RES = (1600, 830)
+GAME_FPS = 60
+
+BG_IMG = "assets/nasa-rTZW4f02zY8-unsplash.jpg"
+SH_IMG = "assets/playerShip2_blue.png"
+MET_1_IMG = "assets/ufoRed.png"
+
+SH_ROTATION = 360
+SH_SPEED = (8, 8)
+SH_POS = (450, 265) 
+
+AST_SPEED = (8, 8)
+AST_ANGLE = 360
+AST_POS = None
+
+FONT_TYPE="comicsans"
+FONT_SIZE=50
+FONT_COLOR=(255, 255, 255)
+
+BG_BASE_POS=(-666,-666)
+
+METEOR_BASE_COUNT=4
+METEOR_COUNT_INCREMENT=4
+METEOR_BASE_SPEED=1
+METEOR_SPEED_INCREMENT=1
---
+++ main.py
@@ -0,0 +1,56 @@
+import pygame
+import sys
+
+
+from config import consts
+from src.game_element import GameElement
+from src.ship import Ship
+from src.meteors import Meteors
+
+
+if __name__ =="__main__":
+    score = 0
+    end = False
+    pygame.init()
+    clock = pygame.time.Clock()
+    window = pygame.display.set_mode(consts.GAME_RES)
+
+    bg = GameElement(consts.BG_BASE_POS, consts.BG_IMG)
+    ship = Ship(consts.SH_POS, consts.SH_IMG)
+    meteors = Meteors(
+        consts.METEOR_BASE_COUNT,
+        consts.METEOR_BASE_SPEED,
+        consts.METEOR_COUNT_INCREMENT,
+        consts.METEOR_SPEED_INCREMENT,
+        consts.MET_1_IMG ,
+        ship.image.get_size(),
+        consts.GAME_RES
+    )
+    game_font = pygame.font.SysFont(consts.FONT_TYPE, consts.FONT_SIZE)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+        keys = pygame.key.get_pressed()
+        ship.handle_keys(keys, consts.SH_SPEED, consts.GAME_RES[0])
+
+        bg.draw(window)
+        ship.draw(window)
+
+        if end:
+            end_text = game_font.render("GAME OVER", True, consts.FONT_COLOR)
+            window.blit(end_text, (int(consts.GAME_RES[0]/2) - int(end_text.get_width()/2), int(consts.GAME_RES[1]/2)))
+        else:
+            score_increment = meteors.move()
+            meteors.draw(window)
+            end = meteors.check_colision(ship)
+
+       
+        meteors.draw(window)
+
+        pygame.display.update()
+        clock.tick(consts.GAME_FPS)
